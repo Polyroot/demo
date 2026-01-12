c3a0b9d3552f25124ceea47848e77e180391770d
Резов Александр Сергеевич <ARezov@alfabank.ru>
2026-01-12 09:02:39 +0300

    implement openapi

--- api/src/main/java/com/example/computershop/HelloApiImpl.java
+++ api/src/main/java/com/example/computershop/HelloApiImpl.java
@@ -2,7 +2,18 @@ package com.example.computershop;
 
 
 import com.example.specs.generated.api.HelloApi;
+import com.example.specs.generated.model.HelloResponseDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HelloApiImpl implements HelloApi {
+
+    @Override
+    public ResponseEntity<HelloResponseDto> getHello() {
+        HelloResponseDto response = new HelloResponseDto();
+        response.setMessage("Hello buddy");
+        return ResponseEntity.ok(response);
+    }
 
 }
